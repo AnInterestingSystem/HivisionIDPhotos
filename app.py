8e7e762b51d18b56d7130fed924017c06228d656
Kai <eggsalloverthere@163.com>
2024-10-18 16:34:35 +0800

    Removed font

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from ui.ui import create_ui
 LANGUAGE = ["zh", "en", "ko", "ja"]
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
-demo = gr.Blocks(title="HivisionIDPhotos", css_paths=[os.path.join(root_dir, "ui/assets/styles.css")], theme=theme, delete_cache=(86400, 86400))
+demo = gr.Blocks(title="iKnow", css_paths=[os.path.join(root_dir, "ui/assets/styles.css")], theme=theme, delete_cache=(86400, 86400))
 processor = IDPhotoProcessor()
 create_ui(demo, processor, root_dir, LANGUAGE)
 
@@ -18,11 +18,10 @@ if __name__ == "__main__":
     argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument("--port", type=int, default=7860, help="The port number of the server")
     argument_parser.add_argument("--host", type=str, default="0.0.0.0", help="The host of the server")
-    argument_parser.add_argument("--root_path", type=str, default=None, help="The root path of the server, default is None (='/'), e.g. '/myapp'")
     args = argument_parser.parse_args()
 
     # 如果RUN_MODE是Beast，打印已开启野兽模式
     if os.getenv("RUN_MODE") == "beast":
         print("[Beast mode activated.] 已开启野兽模式。")
 
-    demo.launch(server_name=args.host, server_port=args.port, root_path=args.root_path, show_api=False, max_file_size="10mb")
+    demo.launch(server_name=args.host, server_port=args.port, show_api=False, max_file_size="10mb")
