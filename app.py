6b7b5fb35d7d42298fd6bf0ff85e55e22b857702
Kai <eggsalloverthere@163.com>
2024-10-23 14:39:15 +0800

    Added static file

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import argparse
 import os
 
 import gradio as gr
+import uvicorn
+from fastapi import FastAPI
+from starlette.responses import FileResponse
 
 from ui.processor import IDPhotoProcessor
 from ui.theme import theme
@@ -10,9 +13,18 @@ from ui.ui import create_ui
 LANGUAGE = ["zh", "en", "ko", "ja"]
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
-demo = gr.Blocks(title="iKnow", css_paths=[os.path.join(root_dir, "ui/assets/styles.css")], theme=theme, delete_cache=(86400, 86400))
+
+app = FastAPI()
+
+
+@app.get("/AXanVx083s.txt")
+async def get_text_file():
+    return FileResponse(os.path.join(root_dir, "ui/assets/AXanVx083s.txt"))
+
+
+blocks = gr.Blocks(title="iKnow", css_paths=[os.path.join(root_dir, "ui/assets/styles.css")], theme=theme, delete_cache=(86400, 86400))
 processor = IDPhotoProcessor()
-create_ui(demo, processor, root_dir, LANGUAGE)
+create_ui(blocks, processor, root_dir, LANGUAGE)
 
 if __name__ == "__main__":
     argument_parser = argparse.ArgumentParser()
@@ -24,4 +36,6 @@ if __name__ == "__main__":
     if os.getenv("RUN_MODE") == "beast":
         print("[Beast mode activated.] 已开启野兽模式。")
 
-    demo.launch(server_name=args.host, server_port=args.port, favicon_path=os.path.join(root_dir, "ui/assets/favicon.ico"), show_api=False, max_file_size="10mb")
+    # blocks.launch(server_name=args.host, server_port=args.port, favicon_path=os.path.join(root_dir, "ui/assets/favicon.ico"), show_api=False, max_file_size="10mb")
+    app = gr.mount_gradio_app(app, blocks, path="/", favicon_path=os.path.join(root_dir, "ui/assets/favicon.ico"), max_file_size="10mb")
+    uvicorn.run(app, host=args.host, port=args.port)
